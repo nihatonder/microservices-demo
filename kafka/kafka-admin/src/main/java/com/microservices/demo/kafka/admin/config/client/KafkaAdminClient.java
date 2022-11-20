ad03649b13d450f67b4d5affd9cf5d640644a94a
Nihat Onder <nihatonder87@gmail.com>
2022-11-20 21:22:39 +0300

    Added retry mechanism template

--- kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/client/KafkaAdminClient.java
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/client/KafkaAdminClient.java
@@ -3,8 +3,10 @@ package com.microservices.demo.kafka.admin.config.client;
 import com.microservices.demo.config.KafkaConfigData;
 import com.microservices.demo.config.RetryConfigData;
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.retry.RetryContext;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 
@@ -29,5 +31,14 @@ public class KafkaAdminClient {
         this.retryConfigData = retryConfigData;
         this.adminClient = client;
         this.retryTemplate = template;
+    }
+
+    public void createTopics() {
+        CreateTopicsResult createTopicsResult;
+        createTopicsResult = retryTemplate.execute(this::doCreateTopics);
+    }
+
+    private CreateTopicsResult doCreateTopics(RetryContext retryContext) {
+        return null;
     }
 }
